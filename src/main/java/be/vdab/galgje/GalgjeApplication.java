ba099a0408f5ff86a6e9003aa1376787dce86a1d
DoganHaluk <halukdogan78@gmail.com>
2021-12-06 02:59:27 +0100

    Initial commit

---
+++ src/main/java/be/vdab/galgje/GalgjeApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.galgje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalgjeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalgjeApplication.class, args);
+    }
+
+}
