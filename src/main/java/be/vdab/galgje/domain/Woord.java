a02ad49719e51b6f883f66d987ee1006e1322c14
DoganHaluk <halukdogan78@gmail.com>
2021-12-16 23:47:22 +0100

    heroku properties added

--- src/main/java/be/vdab/galgje/domain/Woord.java
+++ src/main/java/be/vdab/galgje/domain/Woord.java
@@ -3,6 +3,7 @@ package be.vdab.galgje.domain;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 @Access(AccessType.FIELD)
@@ -19,5 +20,18 @@ public class Woord {
 
     public Woord(String woord) {
         this.woord = woord;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Woord)) return false;
+        Woord woord1 = (Woord) o;
+        return Objects.equals(woord, woord1.woord);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(woord);
     }
 }
